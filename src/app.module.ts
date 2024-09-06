ff2cd55e6fa2cf41a0f3790854643d4fb85c09ef
Pedro Henrique N. Melo <pedro.henrique@gestaokaizen.com.br>
2024-09-05 21:33:56 -0300

    feat: first post user controller

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [PostModule, UserModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
